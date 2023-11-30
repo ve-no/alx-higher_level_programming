165d767825414f9dda301f9009551ca2802652d5
ve-no <allal.elbakourii@gmail.com>
2023-11-30 18:15:59 +0100

    Add script to print command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv) - 1
+
+    print("{} arguments:".format(a))
+
+    if a >= 1:
+        a = 0
+        for arg in sys.argv:
+            if a != 0:
+                print("{}: {}".format(a, arg))
+            a += 1
