21df35509e4b6ec6a5d78076f096ae3aa4b804ac
ve-no <allal.elbakourii@gmail.com>
2023-11-30 18:22:14 +0100

    Fix argument count condition in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     import sys
     a = len(sys.argv) - 1
 
-    if a == 1:
+    if a == 0:
+        print("{} arguments.".format(a))
+    elif a == 1:
         print("{} argument:".format(a))
     else:
         print("{} arguments:".format(a))
