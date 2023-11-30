b70e5a0920a3c396421c7e30cf91c59cc025856a
ve-no <allal.elbakourii@gmail.com>
2023-11-30 18:20:56 +0100

    Refactor argument handling in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,7 @@ if __name__ == "__main__":
     import sys
     a = len(sys.argv) - 1
 
-    if a == 0:
-        print("{} arguments.".format(a))
-    elif a == 1:
+    if a == 1:
         print("{} argument:".format(a))
     else:
         print("{} arguments:".format(a))
