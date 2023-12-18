cd69f744c3366c02a986b90d5ee1691959e1d796
ve-no <allal.elbakourii@gmail.com>
2023-12-18 12:45:30 +0100

    Fix safe_print_list function to correctly count and print elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+	a = 0
 	for i in range(x):
 		try:
 			print(my_list[i], end="")
+			a += 1
 		except IndexError:
-			print()
-			return i
+			break
 	print()
-	return i + 1
+	return a
